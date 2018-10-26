7332d0b5c9f205f7f339c4a6ca862dcd0c1cc2a6
Lucas Miguel <luc.miguel@outlook.com>
2018-10-26 16:24:04 -0300

    cin and cout handler

--- include/evaluator.h
+++ include/evaluator.h
@@ -27,21 +27,21 @@ class Evaluator{
 		~Evaluator(void) = default;
 
 		//support functions
-		bool is_operator(Token);
-		bool is_operand(Token);
-		bool is_opening_scope(symbol);
-		bool is_closing_scope(symbol);
-		bool is_right_association(symbol);
-		short get_precedence(symbol);
-		bool higher_precedence(symbol, symbol);
+		// bool is_operator(Token);
+		// bool is_operand(Token);
+		// bool is_opening_scope(symbol);
+		// bool is_closing_scope(symbol);
+		// bool is_right_association(symbol);
+		// short get_precedence(symbol);
+		// bool higher_precedence(symbol, symbol);
 
 		//main functions
-		std::vector<symbol> infix_to_postfix(std::vector<Token>);
-		value_t evaluate_postfix(std::string);
+		// std::vector<symbol> infix_to_postfix(std::vector<Token>);
+		// value_t evaluate_postfix(std::string);
 
 		//math function
-		value_t pow(value_t, value_t);
-		value_t execute_operator(value_t, value_t, symbol);
+		// value_t pow(value_t, value_t);
+		// value_t execute_operator(value_t, value_t, symbol);
 
 		//getters and setters
 		// std::vector<Token> const get_tokens(void);
--- src/bares.cpp
+++ src/bares.cpp
@@ -3,36 +3,56 @@
 #include "../include/evaluator.h"
 #include "../include/file.h"
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[]){  
 
-    if(argc <= 2){
-        std::cout << "\nERROR: missing file\n";
-        std::cout << "\nAborting...\n" << std::endl;
-        exit(-1);
-    }
-
-    File file(argv[1], argv[2]);
     Parser parser;
-    Evaluator evaluator;
+    //Evaluator evaluator;
 
     std::string expression;
     std::vector<std::string> postfix;
 
-    while(! file.is_eof()){
-        expression = file.read_line();
-        auto result = parser.parse(expression);
+    if(argc <= 2){
 
-        if(result.type != Parser::ResultType::OK){
-            file.write_line(parser.error_msg(result));
+        std::vector<std::string> expressions;
 
-        }else{
-            postfix = evaluator.infix_to_postfix(parser.get_tokens());
-            //postfix = parser.get_tokens();
-        } 
+        while(std::getline(std::cin, expression)){
+            expressions.push_back(expression);
+        }
+
+        std::cout << std::endl;
+
+        for(auto expression : expressions){
+            auto result = parser.parse(expression);
+
+            if(result.type != Parser::ResultType::OK){
+                std::cout << parser.error_msg(result) << std::endl;
+
+            }else{
+                //postfix = evaluator.infix_to_postfix(parser.get_tokens());
+                //postfix = parser.get_tokens();
+            } 
+            
+        }
+
+    }else{
+
+        File file(argv[1], argv[2]);
+
+        while(! file.is_eof()){
+            expression = file.read_line();
+            auto result = parser.parse(expression);
+
+            if(result.type != Parser::ResultType::OK){
+                file.write_line(parser.error_msg(result));
+
+            }else{
+                //postfix = evaluator.infix_to_postfix(parser.get_tokens());
+                //postfix = parser.get_tokens();
+            } 
+
+        }
 
     }
-
-
 
     return EXIT_SUCCESS;
 }
